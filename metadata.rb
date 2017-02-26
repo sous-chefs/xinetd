071713cf1a08dc9767f2cab822087325500041b4
Steven Danna <steve@chef.io>
2017-02-26 15:06:27 +0000

    Bump version to 1.0.2
    Signed-off-by: Steven Danna <steve@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steve@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures xinetd"
 long_description "Installs and configures the xinetd internet service daemon.  Also provides an LWRP for configuring xinetd services."
-version          "1.0.1"
+version          "1.0.2"
 
 supports "ubuntu"
 supports "centos"
